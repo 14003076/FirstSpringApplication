33156b2a4591e2601e480bf1238602af647c9d23
Shaik <shaikshabzan@gmail.com>
2019-01-09 19:00:35 +0530

    Adds FirstSpringApplication

---
+++ src/main/java/com/capgemini/app/App.java
@@ -0,0 +1,27 @@
+package com.capgemini.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.capgemini.app.bean.Organization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+       Organization organization = (Organization) context.getBean("organization");
+      System.out.println(organization.getBoardMembers());
+       System.out.println(organization.getBranchManagers());
+      System.out.println(organization.getCities());
+      System.out.println( organization.getName());
+       System.out.println(organization.getOrgId());
+       System.out.println(organization.getShareValue());
+       System.out.println(organization.isListed());
+       System.out.println(organization.getIpAddresses());    
+    }
+}
